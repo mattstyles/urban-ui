005dc7d8bc2d647c2bcd5ba140fa98e8969f411e
Matt Styles <matt@personalurban.com>
2023-03-01 15:00:01 +0000

    add testground app

---
+++ apps/testground/pages/index.tsx
@@ -0,0 +1,11 @@
+import {Spacer} from '@urban-ui/spacer'
+
+export default function Docs() {
+  return (
+    <div>
+      <h1>Testground</h1>
+      <p>Dev environment for mucking with components</p>
+      <Spacer str='Hello spacer' />
+    </div>
+  )
+}
