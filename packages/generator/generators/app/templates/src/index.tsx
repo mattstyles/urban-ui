e3a601ff1bbce7d1a4188d20ed943b3fe1a52527
Matt Styles <matt@personalurban.com>
2023-03-02 07:08:36 +0000

    fix workaround for TS bug

--- packages/generator/generators/app/templates/src/index.tsx
+++ packages/generator/generators/app/templates/src/index.tsx
@@ -1,3 +1,7 @@
+// https://github.com/microsoft/TypeScript/issues/48212, https://github.com/stitchesjs/stitches/issues/1055#issuecomment-1243663948
+import type {} from '@stitches/react'
+import {styled} from '@urban-ui/theme'
+
 type Props = {
   children: React.ReactNode
 }
