16db66a0f7f28860906cab5c82de0879a7b01177
Matt Styles <matt@personalurban.com>
2023-10-03 09:18:11 +0100

    update generator component scaffold

--- packages/generator/generators/app/templates/src/component.tsx
+++ packages/generator/generators/app/templates/src/component.tsx
@@ -1,6 +1,38 @@
+import type {VariantProps} from 'cva'
 
+import {
+  forwardRef,
+} from 'react'
+import {cva} from 'cva'
+import cx from 'clsx'
+import {atoms} from '@urban-ui/theme/atoms'
+import {Flex} from '@urban-ui/flex'
+import {base} from './input.css.ts'
+import {sizes} from './variants.css.ts'
 
-export interface <%= exportName %>Props extends React.PropsWithChildren {}
-export function <%= exportName %>({children}: Props) {
+const variants = cva([base], {
+  variants: {
+    size: {
+      sm: sizes.sm,
+      md: sizes.md,
+      lg: sizes.lg
+    }
+  },
+  defaultVariants: {
+    size: 'md'
+  }
+})
+
+export interface <%= exportName %>Props extends Variants<typeof variants>, React.PropsWithChildren {}
+type ElementType = HTMLElement
+
+export const <%= exportName %> = forwardRef<ElementType, <%= exportName %>Props>(({size, className, ...props}, ref) => {
+  return (
+    <Flex className={variants({size, className})}>
+      {props.children}
+    </Flex>
+  )
+})
+export function <%= exportName %>({children}: <%= exportName %>Props) {
   return <div data-testid='some-test-id'>{children}</div>
 }
