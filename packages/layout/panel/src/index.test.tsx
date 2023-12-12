51b8a5d221d8ee144b26346cc4e45a26ae0827ee
Matt Styles <matt@personalurban.com>
2023-12-12 06:24:29 +0000

    fix vitest and testing lib interop

--- packages/layout/panel/src/index.test.tsx
+++ packages/layout/panel/src/index.test.tsx
@@ -1,12 +1,16 @@
 import {describe, test, expect} from 'vitest'
 import {render, screen} from '@testing-library/react'
+// jest-dom doesn't seem to understand non-globals and fails
+// import '@testing-library/jest-dom'
+// vitest-dom works though, but recommends using jest-dom
+import 'vitest-dom/extend-expect'
 
 import {Panel} from './index.tsx'
 
 describe('[Panel]', () => {
   test('Forwards the testid', async () => {
     // ARRANGE
-    render(<Panel data-testid='some-id' />)
+    render(<Panel.Root data-testid='some-id' />)
 
     // ACT
 
