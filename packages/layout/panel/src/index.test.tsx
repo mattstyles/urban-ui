c755e710986dc6fd01a826b2ab7ddc5d82ee6a69
Matt Styles <matt@personalurban.com>
2023-12-12 07:44:33 +0000

    update testing library matchers and remove redundancy

--- packages/layout/panel/src/index.test.tsx
+++ packages/layout/panel/src/index.test.tsx
@@ -1,9 +1,9 @@
 import {describe, test, expect} from 'vitest'
 import {render, screen} from '@testing-library/react'
-// jest-dom doesn't seem to understand non-globals and fails
-// import '@testing-library/jest-dom'
+// jest-dom doesn't seem to understand non-globals and fails, v5 fails, v6 is all gucci
+import '@testing-library/jest-dom/vitest'
 // vitest-dom works though, but recommends using jest-dom
-import 'vitest-dom/extend-expect'
+// import 'vitest-dom/extend-expect'
 
 import {Panel} from './index.tsx'
 
