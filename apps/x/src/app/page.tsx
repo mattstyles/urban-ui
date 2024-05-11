0e083ecc5690a4baa341a90d3def034937055476
Matt Styles <matt@personalurban.com>
2024-05-11 06:21:22 +0100

    add modal experiment

--- apps/x/src/app/page.tsx
+++ apps/x/src/app/page.tsx
@@ -1,10 +1,12 @@
-import { Content } from "./content.tsx";
+'use client'
+
+import { Content } from './content.tsx'
 
 export default function Home() {
-	return (
-		<main>
-			<h1>Nextjs stylex esm testing</h1>
-			<Content />
-		</main>
-	);
+  return (
+    <main>
+      <h1>Nextjs stylex esm testing</h1>
+      <Content />
+    </main>
+  )
 }
