26e3aef08979a1223f5745f3a481afd80bdcefa7
Matt Styles <matt@personalurban.com>
2024-07-07 15:36:25 +0100

    update experiment with stylex cli

--- apps/x/src/app/flex/page.tsx
+++ apps/x/src/app/flex/page.tsx
@@ -1,39 +1,47 @@
-import stylex from "@stylexjs/stylex";
-import { Flex } from "@urban-ui/flex";
-import { spacing } from "@urban-ui/theme/spacing.stylex";
+import stylex from '@stylexjs/stylex'
+import { Flex } from '@urban-ui/flex'
+import { spacing } from '@urban-ui/theme/spacing.stylex'
 
 const styles = stylex.create({
-	block: {
-		backgroundColor: "hotpink",
-		width: spacing.xl,
-		height: spacing.xl,
-	},
-});
+  block: {
+    backgroundColor: 'hotpink',
+    width: spacing.xl,
+    height: spacing.xl,
+  },
+})
 
 function Block() {
-	return <div {...stylex.props(styles.block)} />;
+  return <div {...stylex.props(styles.block)} />
 }
 
 export default function FlexPage() {
-	return (
-		<>
-			<h1>Flex</h1>
-			<Flex orientation="v" gap="xl">
-				<div>gap: sm</div>
-				<Flex gap="sm">
-					<Block />
-					<Block />
-					<Block />
-					<Block />
-				</Flex>
-				<div>gap: md</div>
-				<Flex gap="md">
-					<Block />
-					<Block />
-					<Block />
-					<Block />
-				</Flex>
-			</Flex>
-		</>
-	);
+  return (
+    <>
+      <h1>Flex</h1>
+      <Flex orientation="v" gap="sm">
+        <div>gap: sm</div>
+        <Flex gap="sm">
+          <Block />
+          <Block />
+          <Block />
+          <Block />
+        </Flex>
+        <div>gap: md</div>
+        <Flex gap="md">
+          <Block />
+          <Block />
+          <Block />
+          <Block />
+        </Flex>
+        <div>gap: lg</div>
+        <Flex gap="lg">
+          <Block />
+          <Block />
+          <Block />
+          <Block />
+        </Flex>
+      </Flex>
+      <div style={{ paddingBottom: 24 }} />
+    </>
+  )
 }
--- packages/layout/flex/arc.config.ts
+++ packages/layout/flex/arc.config.ts
@@ -1,5 +1,10 @@
+import cp from 'node:child_process'
+import path from 'node:path'
+import util from 'node:util'
 import type { Config } from '@urban-ui/arc'
 import { getTsConfig } from '@urban-ui/arc/ts'
+
+// const spawn = util.promisify(cp.spawn)
 
 const tsconfig = await getTsConfig()
 
@@ -7,6 +12,40 @@ const config: Config = {
   include: [tsconfig.compilerOptions?.rootDir ?? 'src', '!**/*.test.ts*'],
   outDir: 'dist',
   rootDir: tsconfig.compilerOptions?.rootDir ?? 'src',
+  events: {
+    complete: async () => {
+      console.log('🎉')
+      await spawn('del', ['disttest2'])
+      console.log(process.cwd(), __dirname)
+      await spawn('stylex', [
+        '--config',
+        path.join(__dirname, 'stylex.config.jsonc'),
+      ])
+      // await new Promise((res) => {
+      //   const pipe = cp.spawn('stylex', ['-i', 'disttest', '-o', 'disttest2'], {
+      //     stdio: [0, 1, 2],
+      //   })
+      //   pipe.on('data', (chunk) => {
+      //     console.log(chunk)
+      //   })
+      //   pipe.on('close', () => {
+      //     console.log('👩‍🎤')
+      //     res(null)
+      //   })
+      // })
+      console.log('❤️')
+    },
+  },
 }
 
 export default config
+
+function spawn(cmd: string, params: Array<string>) {
+  return new Promise((res) => {
+    const pipe = cp.spawn(cmd, params, {
+      stdio: [0, 1, 2],
+    })
+    pipe.on('data', console.log)
+    pipe.on('close', res)
+  })
+}
