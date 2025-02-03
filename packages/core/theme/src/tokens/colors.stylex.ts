4a7b352bb21b17961e2e72370d36d9dab7b25535
Matt Styles <matt@personalurban.com>
2025-02-03 12:31:37 +0000

    add :rocket: dark mode variables

--- packages/core/theme/src/tokens/colors.stylex.ts
+++ packages/core/theme/src/tokens/colors.stylex.ts
@@ -1,34 +1,102 @@
 import { defineVars } from '@stylexjs/stylex'
 
+const DARK = '@media (prefers-color-scheme: dark)'
+
 export const baseColors = defineVars({
-  transparent: 'transparent',
-  current: 'currentColor',
-  black: 'hsl(0 0% 0%)',
-  white: 'hsl(0 0% 100%)',
+  transparent: { default: 'transparent' },
+  current: { default: 'currentColor' },
+  black: {
+    default: 'hsl(0 0% 0%)',
+    [DARK]: 'hsl(0 0% 100%)'
+  },
+  white: {
+    default: 'hsl(0 0% 100%)',
+    [DARK]: 'hsl(0 0% 0%)'
+  }
 })
 
 export const grays = defineVars({
-  50: 'hsl(0 0% 98%)',
-  100: 'hsl(0 0% 96%)',
-  200: 'hsl(0 0% 90%)',
-  300: 'hsl(0 0% 83%)',
-  400: 'hsl(0 0% 74%)',
-  500: 'hsl(0 0% 62%)',
-  600: 'hsl(0 0% 46%)',
-  700: 'hsl(0 0% 38%)',
-  800: 'hsl(0 0% 26%)',
-  900: 'hsl(0 0% 13%)',
+  50: {
+    default: 'hsl(0 0% 98%)',
+    [DARK]: 'hsl(0 0% 13%)'
+  },
+  100: {
+    default: 'hsl(0 0% 96%)',
+    [DARK]: 'hsl(0 0% 26%)'
+  },
+  200: {
+    default: 'hsl(0 0% 90%)',
+    [DARK]: 'hsl(0 0% 38%)'
+  },
+  300: {
+    default: 'hsl(0 0% 83%)',
+    [DARK]: 'hsl(0 0% 46%)'
+  },
+  400: {
+    default: 'hsl(0 0% 74%)',
+    [DARK]: 'hsl(0 0% 62%)'
+  },
+  500: {
+    default: 'hsl(0 0% 62%)',
+    [DARK]: 'hsl(0 0% 74%)'
+  },
+  600: {
+    default: 'hsl(0 0% 46%)',
+    [DARK]: 'hsl(0 0% 83%)'
+  },
+  700: {
+    default: 'hsl(0 0% 38%)',
+    [DARK]: 'hsl(0 0% 90%)'
+  },
+  800: {
+    default: 'hsl(0 0% 26%)',
+    [DARK]: 'hsl(0 0% 96%)'
+  },
+  900: {
+    default: 'hsl(0 0% 13%)',
+    [DARK]: 'hsl(0 0% 98%)'
+  }
 })
 
 export const primary = defineVars({
-  50: 'hsl(270 100% 98%)',
-  100: 'hsl(270 100% 92%)',
-  200: 'hsl(270 100% 86%)',
-  300: 'hsl(270 100% 75%)',
-  400: 'hsl(270 100% 65%)',
-  500: 'hsl(270 100% 50%)',
-  600: 'hsl(270 100% 40%)',
-  700: 'hsl(270 100% 35%)',
-  800: 'hsl(270 100% 30%)',
-  900: 'hsl(270 100% 25%)',
+  50: {
+    default: 'hsl(270 100% 98%)',
+    [DARK]: 'hsl(270 100% 25%)'
+  },
+  100: {
+    default: 'hsl(270 100% 92%)',
+    [DARK]: 'hsl(270 100% 30%)'
+  },
+  200: {
+    default: 'hsl(270 100% 86%)',
+    [DARK]: 'hsl(270 100% 35%)'
+  },
+  300: {
+    default: 'hsl(270 100% 75%)',
+    [DARK]: 'hsl(270 100% 40%)'
+  },
+  400: {
+    default: 'hsl(270 100% 65%)',
+    [DARK]: 'hsl(270 100% 50%)'
+  },
+  500: {
+    default: 'hsl(270 100% 50%)',
+    [DARK]: 'hsl(270 100% 65%)'
+  },
+  600: {
+    default: 'hsl(270 100% 40%)',
+    [DARK]: 'hsl(270 100% 75%)'
+  },
+  700: {
+    default: 'hsl(270 100% 35%)',
+    [DARK]: 'hsl(270 100% 86%)'
+  },
+  800: {
+    default: 'hsl(270 100% 30%)',
+    [DARK]: 'hsl(270 100% 92%)'
+  },
+  900: {
+    default: 'hsl(270 100% 25%)',
+    [DARK]: 'hsl(270 100% 98%)'
+  }
 })
