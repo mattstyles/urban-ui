497fa26ce528740f20427b4cf866664115ec0f1c
Matt Styles <matt@personalurban.com>
2025-04-02 08:04:51 +0100

    add :rocket: bundle analyser

--- apps/docs/next.config.js
+++ apps/docs/next.config.js
@@ -2,6 +2,7 @@
 
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
+import createAnalyzerPlugin from '@next/bundle-analyzer'
 // import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
 import createStyleXPlugin from '@stylexswc/nextjs-plugin'
@@ -66,12 +67,16 @@ const withStyleX = createStyleXPlugin({
   extractCSS: false,
 })
 
+const withBundleAnalyzer = createAnalyzerPlugin({
+  enabled: process.env.ANALYZE === 'true',
+})
+
 // module.exports = withStyleXPlugin(withMDX(config))
 // module.exports = withMDX(config)
 // export default withStyleXPlugin(withVanillaExtract(withMDX(config)))
 // export default withVanillaExtract(withMDX(config))
 
-export default withStyleX(config)
+export default withBundleAnalyzer(withStyleX(config))
 // export default withMDX(withStyleX(config))
 // module.exports = withMDX(withStyleX(config))
 // module.exports = withStyleX(config)
