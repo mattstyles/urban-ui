95bdf5f17e779694500bbcdde4ed9a3a3d20c77f
Matt Styles <matt@personalurban.com>
2026-01-18 15:22:58 +0000

    :suspect: tsconfig and test config

--- packages/action/button/src/button.test.tsx
+++ packages/action/button/src/button.test.tsx
@@ -1,4 +1,4 @@
-import { describe, expect, it } from '@jest/globals'
+// import { describe, expect, it } from '@jest/globals'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
