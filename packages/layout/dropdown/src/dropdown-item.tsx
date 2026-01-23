b97165e50e2753f39598af537321b939e1b1e61e
Matt Styles <matt@personalurban.com>
2026-01-23 09:58:11 +0000

    :wrench: remove debug text from dropdown item

--- packages/layout/dropdown/src/dropdown-item.tsx
+++ packages/layout/dropdown/src/dropdown-item.tsx
@@ -105,7 +105,7 @@ export function DropdownItem<T extends object>({
             {children}
           </DropdownItemTextInline>
         ) : (
-          <>{children} foo</>
+          children
         ),
       )}
     </AriaListBoxItem>
